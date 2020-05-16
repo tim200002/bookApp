dd81c1037e0ee7bdc7340d818fc46e7c6d08ef01
Tim <tim.lindi@outlook.de>
2020-05-16 18:08:28 +0200

    Some More Fixes

--- lib/Bloc/newBookBloc.dart
+++ lib/Bloc/newBookBloc.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:developer';
 
 import 'package:book_app/Event/newBookEvents.dart';
@@ -9,26 +7,31 @@ import 'package:book_app/model/book.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 //Bloc whicht Creates New book
-class BlocNewBook extends Bloc<NewBookEvents, NewBookStates>{
-      var repository=MainRepository();
-  
-  @override
-  NewBookStates get initialState =>EnterISBN();
+class BlocNewBook extends Bloc<NewBookEvents, NewBookStates> {
+  var repository = MainRepository();
 
   @override
-  Stream<NewBookStates> mapEventToState(NewBookEvents event)async*{
+  NewBookStates get initialState => EnterISBN();
 
+  @override
+  Stream<NewBookStates> mapEventToState(NewBookEvents event) async* {
     //Lookup Book and Add it to the DB, later not directly add it
-    if (event is EventLookForBook){
-     int pages=(await repository.addBookByIsbn(event.ISBN)).pages;
-     pages=pages~/365;
-     if (pages==0) pages=1;
-     await repository.updateTodaysToRead(pages);
+    if (event is EventLookForBook) {
+      try {
+        int pages = (await repository.addBookByIsbn(event.ISBN)).pages;
+        int daysTillNewYear=- DateTime.now().difference(DateTime(DateTime.now().year, 12,31)).inDays;
+        log("Days Till New Year");
+        log(daysTillNewYear.toString());
+        pages = (pages / daysTillNewYear).ceil();
+        if (pages == 0) pages = 1;
+        await repository.updateTodaysToRead(pages);
+      } catch (err) {
+        log(err.toString());
+      }
     }
     //Gets Deleted later
-    if (event is TestEvent){
+    if (event is TestEvent) {
       await repository.deleteALL();
       log("deleted");
     }
